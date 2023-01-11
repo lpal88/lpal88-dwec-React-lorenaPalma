5f1d79c538f767f4051b38131d5cefa8e53fb5d4
lpal88 <lpalpar893@g.educaand.es>
2023-01-11 18:38:50 +0100

    apiCall con filtro por tag

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,18 @@
 import React,  { useState } from 'react'
 
 const SearchBar = () => {
+  
+  const inputsInitialState = {
+    author: '',
+    tag: 'all',
+  }
 
-  const [author, setAuthor] = useState("")
+  const [inputs, setInputs] = useState(inputsInitialState)
+
   const [dataAuthor, setDataAuthor] = useState([])
   const [dataQuotes, setDataQuotes] = useState([])
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,16 +22,21 @@ const SearchBar = () => {
   
   const handleChange = e => {
     // console.log(e.target.value)
-    setAuthor(e.target.value)
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    })
+
   }
 
   function searchAuthor() {
-        APIcall(author)
+        APIcall(inputs)
   }
   
-  function APIcall(author) {
-    let urlAuthor = `https://api.quotable.io/search/authors?query=${author}`;
-    let urlQuotes = `https://api.quotable.io/quotes?author=${author}`;
+  function APIcall(inputs) {
+    let urlAuthor = `https://api.quotable.io/search/authors?query=${inputs.author}`;
+    let urlQuotes = `https://api.quotable.io/quotes?author=${inputs.author}`;
+
   
     //mostrarSpinner()
     fetch(urlAuthor)
@@ -42,27 +55,37 @@ const SearchBar = () => {
         //limpiarHTML()
         setDataQuotes(data.results)
       )
+  
   }
+
   const {name, description, bio} = dataAuthor
-  console.log(dataQuotes)
+
+ 
 
 
   return (
     <><form className="section__searchBar" onSubmit={handleSubmit} action="#" method="POST">
-      <h1>Busca tus autorxs favoritxs</h1>
+      <h1>Busca autorxs célebres y crea tu lista de citas favoritas</h1>
       <input
         className="searchBar__box"
-        name='search'
+        name='author'
         type="text"
         placeholder="Aristotle, Confucius..."
         onChange={e => handleChange(e)}
-        value={author} />
+        value={inputs.author} />
       <button type='submit' className="searchBar__link">Buscar</button>
+      
 
-      <select name="topics" className='searchBar__options'>
-        <option value="1" selected>Elige un tema</option>
-        <option value="2">Freedom</option> 
-        <option value="3">Happiness</option>
+      <select 
+      name="tag" 
+      className='searchBar__options'
+      onChange={e =>handleChange(e)}
+      value={inputs.tag}>
+        <option value="all">Cualquier tema</option>
+        <option value="change">Cambio</option> 
+        <option value="wisdom">Sabiduría</option>
+        <option value="freedom">Libertad</option> 
+        <option value="friendship">Amistad</option>
       </select>
       
     </form>
@@ -71,9 +94,18 @@ const SearchBar = () => {
       <h2>{description}</h2>
       <p>{bio}</p>
       <ul>{
-        dataQuotes.map(quote =>
-          ( <li key={quote._id} className="authorMain__qoute">{quote.content}</li> ) 
-        )
+          inputs.tag === 'all'  
+          ? dataQuotes.map(quote => 
+            ( <li key={quote._id} className="authorMain__qoute">{quote.content}</li> ) 
+            )
+          : dataQuotes
+            .filter(quote => 
+              (quote.tags.some(tag => 
+                tag === inputs.tag)
+              ))
+            .map(quote => 
+                ( <li key={quote._id} className="authorMain__qoute">{quote.content}</li> ) 
+              )
       }
       </ul>
     </div></>
